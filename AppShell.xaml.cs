9dd1ec4f824f348251bd7c7e5eb508096ad6ff8a
Angel Pimentel <angelpimentelcontact.@gmail.com>
2025-05-11 12:22:11 -0400

    Add project files.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiAppInterfaz
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace MauiAppInterfaz
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void OnSaludarClicked(object sender, EventArgs e)
+        {
+            string nombre = nombreEntrada.Text;
+            saludoLabel.Text = $"Hola, {nombre} 👋";
+        }
+    }
+
+}
